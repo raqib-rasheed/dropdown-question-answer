cd3e549c5a14ecc2ca207983533cace92473d7ce
Raqib Rasheed <raqib.abdulrasheed@outlook.com>
2021-01-08 14:59:49 +0530

    project completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Questions from './questions';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="content">
+      <h2>General Questions</h2>
+      <Questions />
     </div>
-  );
+    
+  )
 }
 
 export default App;
---
+++ src/questions.jsx
@@ -0,0 +1,38 @@
+
+import React from 'react'
+
+export default function Questions(){
+  return (
+  <>
+<div className="dropdown mt-1">
+  <span className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown">
+    question 1?
+  </span>
+  <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <li><p className="dropdown-item">answer</p></li>
+  </ul>
+</div>
+
+<div className="dropdown mt-1 ">
+  <span className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown">
+    question 1?
+  </span>
+  <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <li><p className="dropdown-item">answer</p></li>
+  </ul>
+</div>
+
+<div className="dropdown mt-1">
+  <span className="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown">
+    question 1?
+  </span>
+  <ul className="dropdown-menu" aria-labelledby="dropdownMenuLink">
+    <li><p className="dropdown-item">answer</p></li>
+  </ul>
+</div>
+  </>
+  )
+}
+
+
+
